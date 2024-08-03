872ff85462ed1382c0a7946efc810ae3efcdd557
krahul09 <kumar.rahul0525x@gmail.com>
2024-08-03 23:59:14 +0530

    fix the issues realted to data not getting fetched with the API requests got expired

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,27 +1,24 @@
 export const exerciseOptions = {
-    method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
-    headers: {
-      'X-RapidAPI-Key': '75d83e435cmsh283d89ea50b0619p1d652ejsn41d729181918' ,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-  };
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": "213f406631msh3b5d74d815d95d7p1d8e72jsnc7ec6bd25644",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
 
-  export const youtubeOptions = {
-    method: 'GET',
-    url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
-    headers: {
-      'X-RapidAPI-Key': '75d83e435cmsh283d89ea50b0619p1d652ejsn41d729181918',
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-    }
-  };
+export const youtubeOptions = {
+  method: "GET",
+  url: "https://youtube-search-and-download.p.rapidapi.com/channel/about",
+  headers: {
+    "X-RapidAPI-Key": "75d83e435cmsh283d89ea50b0619p1d652ejsn41d729181918",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
 
-export const fetchData = async(url , options) => {
-    const response = await fetch(url , options);
-    const data = await response.json();
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
 
-    return data;
-}
-
-
-
+  return data;
+};
